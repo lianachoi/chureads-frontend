332d5c0d41df33d125fdf242037efe46010fb3ae
lianachoi <2ndsunmail@gmail.com>
2025-07-16 16:42:40 +0900

    로그인 및 회원가입 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,28 @@ import Post from "./pages/Post";
 import Profile from "./pages/Profile";
 import Edit from "./pages/Edit";
 import Sample from "./pages/Sample";
+import { useEffect, useState } from "react";
+import { auth } from "./firebase";
 
 function App() {
   // logic
+  // 진입시 무조건 로딩, 인증서비스 준비완료후에 로딩 false
+  const [isLoading, setIsLoading] = useState(true);
+  const init = async() => {
+    await auth.authStateReady() //로그인상태변화감지, await이 끝날때 까지 기다림.
+    console.log("인증완료", auth)
+    setIsLoading(false)
+  };
 
+  useEffect(()=>{
+    init();
+  },[])
   // view
   return (
     <div className="bg-churead-black h-full text-white overflow-auto">
+      {isLoading ? 
+      <p className="text-2xl">Loading..</p>
+      :
       <div className="max-w-[572px] mx-auto h-full">
         <BrowserRouter>
           <Routes>
@@ -25,7 +40,7 @@ function App() {
             <Route path="/sample" element={<Sample />} />
           </Routes>
         </BrowserRouter>
-      </div>
+      </div>}
     </div>
   );
 }
